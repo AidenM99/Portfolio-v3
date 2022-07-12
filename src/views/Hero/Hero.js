c35c07f61d97edeedc0f707111e4f2d93b20e175
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-07-12 15:44:54 +0100

    Add Hero section

---
+++ src/views/Hero/Hero.js
@@ -0,0 +1,12 @@
+import DarkWavyBg from "../../components/Backgrounds/DarkWavyBg";
+
+const Hero = () => {
+  return (
+    <section className="hero-section">
+      <h1 className="hero-name">Aiden McNaughton</h1>
+      <DarkWavyBg />
+    </section>
+  );
+};
+
+export default Hero;
