50be55be6cac6f5adf35195883f89b152355f8ad
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-07-12 15:47:26 +0100

    Add Navbar

---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,13 @@
+import NavItems from "./NavItems";
+
+const NavBar = () => {
+  return (
+    <header>
+      <nav>
+        <NavItems />
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
---
+++ src/components/Navbar/NavItems.js
@@ -0,0 +1,16 @@
+const NavItems = () => {
+  return (
+    <ul className="nav-items">
+      <div className="nav-left">
+        <span className="nav-logo">A</span>
+      </div>
+      <div className="nav-right">
+        <li className="nav-item">Projects</li>
+        <li className="nav-item">About</li>
+        <li className="nav-item">Contact</li>
+      </div>
+    </ul>
+  );
+};
+
+export default NavItems;
