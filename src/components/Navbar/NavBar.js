92d69192cdabfb9f613e7f39001c2e4a42c575f7
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-07-15 17:17:35 +0100

    Add NavDrawer component and animation when opening the drawer

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,11 +1,52 @@
-import NavItems from "./NavItems";
+import NavDrawer from "./NavDrawer";
+import { useEffect, useState } from "react";
+import { Squash as Hamburger } from "hamburger-react";
 
 const NavBar = () => {
+  const navMenuLinks = ["About", "Projects", "Contact"];
+  const [isOpen, setOpen] = useState(false);
+  const mql = window.matchMedia("(min-width: 768px)");
+
+  if (isOpen) {
+    document.body.style.overflow = "hidden";
+    document.body.style.height = "100%";
+  } else {
+    document.body.style.overflow = "auto";
+    document.body.style.height = "auto";
+  }
+
+  useEffect(() => {
+    try {
+      mql.addEventListener("change", () => setOpen(false));
+    } catch (e1) {
+      try {
+        mql.addListener(() => setOpen(false));
+      } catch (e2) {
+        console.error(e2);
+      }
+    }
+  }, [mql]);
+
   return (
     <header>
       <nav>
-        <NavItems />
+        <ul className="nav-items">
+          <div className="nav-left">
+            <span className="nav-logo">
+              <a href="#hero">A</a>
+            </span>
+          </div>
+          <div className="nav-right">
+            {navMenuLinks.map((link, index) => (
+              <li key={index} className="nav-item">
+                <a href={`#${link.toLowerCase()}`}>{link}</a>
+              </li>
+            ))}
+          </div>
+          <Hamburger toggled={isOpen} toggle={setOpen} color="#fff" />
+        </ul>
       </nav>
+      <NavDrawer isOpen={isOpen} setOpen={setOpen} />
     </header>
   );
 };
---
+++ src/components/Navbar/NavDrawer.js
@@ -0,0 +1,68 @@
+import { motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
+
+const NavDrawer = ({ isOpen, setOpen }) => {
+  const navMenuLinks = ["About", "Projects", "Contact"];
+
+  const navVariant = {
+    open: {
+      width: "100%",
+      transition: {
+        duration: 0.2,
+      },
+    },
+    closed: {
+      width: 0,
+      transition: {
+        duration: 0.2,
+      },
+    },
+  };
+
+  const linksVariant = {
+    hidden: {
+      opacity: 0,
+    },
+    closed: {
+      opacity: 0,
+      transition: {
+        duration: 0.1,
+      },
+    },
+    visible: {
+      opacity: 1,
+    },
+  };
+
+  return (
+    <AnimatePresence>
+      {isOpen ? (
+        <motion.div
+          className="nav-menu"
+          variants={navVariant}
+          animate={isOpen ? "open" : null}
+          exit="closed"
+        >
+          <div className="nav-menu-links">
+            {navMenuLinks.map((link, index) => (
+              <motion.span
+                key={link}
+                className="nav-menu-link"
+                variants={linksVariant}
+                initial="hidden"
+                animate={isOpen ? "visible" : null}
+                transition={{ duration: 1 }}
+                exit="closed"
+                onClick={() => setOpen(!isOpen)}
+              >
+                <a href={`#${link.toLowerCase()}`}>{link}</a>
+              </motion.span>
+            ))}
+          </div>
+        </motion.div>
+      ) : null}
+    </AnimatePresence>
+  );
+};
+
+export default NavDrawer;
