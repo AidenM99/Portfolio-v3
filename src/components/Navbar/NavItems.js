1f37dceb15f6299007247c8f755c1b48006a04bb
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-07-15 15:00:02 +0100

    Add functionality for nav links to scroll to relevant section on click

--- src/components/Navbar/NavItems.js
+++ src/components/Navbar/NavItems.js
@@ -4,12 +4,20 @@ const NavItems = () => {
   return (
     <ul className="nav-items">
       <div className="nav-left">
-        <span className="nav-logo">A</span>
+        <a href="#hero">
+          <span className="nav-logo">A</span>
+        </a>
       </div>
       <div className="nav-right">
-        <li className="nav-item">Projects</li>
-        <li className="nav-item">About</li>
-        <li className="nav-item">Contact</li>
+        <a href="#projects">
+          <li className="nav-item">Projects</li>
+        </a>
+        <a href="#about">
+          <li className="nav-item">About</li>
+        </a>
+        <a href="#contact">
+          <li className="nav-item">Contact</li>
+        </a>
         <Hamburger color="#fff" />
       </div>
     </ul>
